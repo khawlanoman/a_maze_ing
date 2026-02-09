c9d35a1d6b73b4ec2602d97d3ba4ea4ed6830544
Sara Sarbout <sasarbou@e1r11p2.1337.ma>
2026-02-09 20:35:16 +0100

    f

--- parsing_config.py
+++ parsing_config.py
@@ -15,52 +15,53 @@ def read_file() -> dict[str, str]:
 
 
 def filter_config_data(config: dict[str, str]) -> dict[str, Any]:
-    try:
-        good_dict = {}
-        for key, value in config.items():
-            if key == 'WIDTH':
-                if int(value) > 0:
-                    good_dict[key] = int(value)
-                else:
-                    raise ValueError("Error: WIDTH value cannot be negative!")
-            elif key == 'HEIGHT':
-                if int(value) > 0:
-                    good_dict[key] = int(value)
-                else:
-                    return "HEIGHT value cannot be negative!"
-            elif key == 'HEIGHT' and int(value) < 0:
-                return "Error: HEIGHT value cannot be negative!"
-            elif key == 'ENTRY':
-                entry_tuple = tuple(int(x) for x in value.split(","))
-                print(entry_tuple)
-                if (len(entry_tuple) == 2):
-                    good_dict[key] = entry_tuple
-                else:
-                    return "Error: Invalid data in 'ENTRY' input in 'config.txt'"
-            elif key == 'EXIT':
-                exit_tuple = tuple(int(y) for y in value.split(","))
-                if (len(exit_tuple) == 2):
-                    good_dict[key] = exit_tuple
-                else:
-                    return "Error: Invalid data in 'EXIT' in 'config.txt'"
-            elif key == 'OUTPUT_FILE':
-                if value == 'maze.txt':
-                    good_dict[key] = value
-                else:
-                    return "Error: Invalid data in 'OUTPUT_FILE' in 'config.txt'"
-            val = value.strip().upper()
-            if key == 'PERFECT':
-                if val == "TRUE":
-                    good_dict[key] = True
-                elif val == "FALSE":
-                    good_dict[key] = False
-                else:
-                    return "Error: Invalid data in 'PERFECT' boolean stats!"
+    good_dict = {}
+    for key, value in config.items():
+        if key == 'WIDTH':
+            try:
+                width = int(value)
+            except ValueError:
+                raise ValueError("'WIDTH' must be an integer")
+            if width <= 0:
+                raise ValueError("'WIDTH' value cannot be negative!")
+            good_dict[key] = width
+        elif key == 'HEIGHT':
+            if int(value) > 0:
+                good_dict[key] = int(value)
             else:
-                continue
+                return "HEIGHT value cannot be negative!"
+        elif key == 'HEIGHT' and int(value) < 0:
+            return "Error: HEIGHT value cannot be negative!"
+        elif key == 'ENTRY':
+            entry_tuple = tuple(int(x) for x in value.split(","))
+            if (len(entry_tuple) == 2):
+                good_dict[key] = entry_tuple
+            else:
+                return "Error: Invalid data in 'ENTRY' input in 'config.txt'"
+        elif key == 'EXIT':
+            exit_tuple = tuple(int(y) for y in value.split(","))
+            if (len(exit_tuple) == 2):
+                good_dict[key] = exit_tuple
+            else:
+                return "Error: Invalid data in 'EXIT' in 'config.txt'"
+        elif key == 'OUTPUT_FILE':
+            if value == 'maze.txt':
+                good_dict[key] = value
+            else:
+                return "Error: Invalid data in 'OUTPUT_FILE' in 'config.txt'"
+        val = value.strip().upper()
+        if key == 'PERFECT':
+            if val == "TRUE":
+                good_dict[key] = True
+            elif val == "FALSE":
+                good_dict[key] = False
+            else:
+                return "Error: Invalid data in 'PERFECT' boolean stats!"
+        else:
+            continue
         return good_dict
-    except ValueError as e:
-        print(f"Error: {e}")
 
-
-print(filter_config_data(read_file()))
+try:
+    print(filter_config_data(read_file()))
+except ValueError as e:
+    print(f"Error: {e}")
